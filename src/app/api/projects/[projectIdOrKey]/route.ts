36c025b14f0240406edb05e4b626f21a8e5d2565
Yana Burdakova <bu.asti@gmail.com>
2025-01-15 10:52:31 -0500

    fix: fix router

--- src/app/api/projects/[projectIdOrKey]/route.ts
+++ src/app/api/projects/[projectIdOrKey]/route.ts
@@ -1,19 +1,28 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { projectIdOrKey: string } }) {
-  const baseUrl = "https://courthousecomputersystems.atlassian.net/rest/api/3";
+export async function GET(
+  request: Request,
+  { params }: { params: { projectIdOrKey: string } }
+) {
+  const { projectIdOrKey } = params;
+
+  if (!projectIdOrKey) {
+    return NextResponse.json({ error: "Project ID or Key is required" }, { status: 400 });
+  }
+
+  const baseUrl = "https://courthousecomputersystems.atlassian.net/rest/api/3/project";
   const email = process.env.NEXT_PUBLIC_JIRA_API_EMAIL;
   const token = process.env.NEXT_PUBLIC_JIRA_API_TOKEN;
 
   if (!email || !token) {
-    console.error("JIRA_API_EMAIL or JIRA_API_TOKEN is not defined");
-    return NextResponse.json({ error: "Email or token not defined" }, { status: 500 });
+    return NextResponse.json(
+      { error: "JIRA_API_EMAIL or JIRA_API_TOKEN is not defined" },
+      { status: 500 }
+    );
   }
 
-  const { projectIdOrKey } = params;
-
   try {
-    const response = await fetch(`${baseUrl}/project/${projectIdOrKey}`, {
+    const response = await fetch(`${baseUrl}/${projectIdOrKey}`, {
       method: "GET",
       headers: {
         Authorization: `Basic ${Buffer.from(`${email}:${token}`).toString("base64")}`,
