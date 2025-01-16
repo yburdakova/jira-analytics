bf7425f8fdd940294d4a566bcbf430ae7491015f
Yana Burdakova <bu.asti@gmail.com>
2025-01-16 12:39:06 -0500

    feat: add btns for analitycs section

--- src/app/projects/[projectIdOrKey]/ProjectPageClient.tsx
+++ src/app/projects/[projectIdOrKey]/ProjectPageClient.tsx
@@ -5,10 +5,13 @@ import { useProjects } from "@/context/ProjectsContext";
 import { useEffect, useState } from "react";
 
 export default function ProjectPageClient() {
-  const { isLoading, currentProject } = useProjects();
+  const { currentProject } = useProjects();
   const [total, setTotal] = useState<number | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [issuesLoading, setIssuesLoading] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+
+  const sections = ["Tickets", "Counties", "Assignees", "Reporters"]
 
   useEffect(() => {
     fetchIssues();
@@ -38,10 +41,12 @@ export default function ProjectPageClient() {
     }
   };
 
-
-  if (isLoading) {
-    return <Loader note="Loading data from context..."></Loader>;
-  }
+  const getAnalytics = (section: string) => {
+    if (section) {
+      setActiveSection(section);
+    }
+    console.log(`Fetching ${section} analytics...`);
+  };
 
   if (error) {
     return <div>Error {error}</div>;
@@ -51,6 +56,27 @@ export default function ProjectPageClient() {
     <div className="flex flex-col gap-2">
       <h1 className="text-2xl font-bold">Current project: {currentProject?.name}</h1>
       {issuesLoading ?<Loader note={`Loading tickets data...`}/>: <div className="">Total tickets: {total}</div>}
+      <div className="mb-4 flex gap-4">
+        {sections.map((section) => (
+          <button
+            key={section}
+            className={`px-4 py-2 rounded ${
+              activeSection === section
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200/50 hover:bg-blue-300/50"
+            }`}
+            onClick={() => getAnalytics(section)}
+          >
+            {section}
+          </button>
+        ))}
+      </div>
+      <div className="p-4">
+        {activeSection === "Tickets" && <div>Tickets content</div>}
+        {activeSection === "Counties" && <div>Counties content</div>}
+        {activeSection === "Assignee" && <div>Assignee content</div>}
+        {activeSection === "Reporters" && <div>Reporters content</div>}
+      </div>
     </div>
   );
 }
